4a7a688029d8ace6ce6a16682aaf815a06621e9d
EthanYang1219 <yangethan2006@gmail.com>
2023-12-07 22:13:55 -0800

    WORKING NEW AWP AUTO, ONLY NEED TO TUNE LAST PART

--- hkh-main/src/main.cpp
+++ hkh-main/src/main.cpp
@@ -71,29 +71,30 @@ void competition_initialize() {}
 void autonomous() {
 	intake = -127;
 	PIDdrive(47,0.22,0.01,8,1350);
-	PIDdrive(-47,0.25,0.01,8,1350);
+	PIDdrive(-46,0.20,0.01,8,1350);
 	delay(200);
-	PIDturn(87,1.9,0.00025,6,1000);
+	PIDturn(90,1.9,0.00025,6,1000);
 	intake = 0;
 	PIDdriverightArc(-40,0.4,0.01,0.2,2200);
-	PIDdrive(5,0.22,0.01,5,1350);
-	PIDdrive(-7,0.25,0.01,5,1350);
-	PIDdrive(13,0.22,0.01,5,1000);
-	PIDturn(-40,1.99,0.00035,7,1000);
+	PIDdrive(14,0.22,0.01,5,1000);
+	PIDturn(-42,1.99,0.00035,7,1000);
 	delay(350);
-	PIDdrive(9,0.5,0.01,5,1000);
+	PIDdrive(12,0.5,0.01,5,1000);//Drive forward to descore triball in corner
+	delay(150);
 	wings.set_value(true);
-	delay(200);
+	delay(150);
 	PIDturn(-45,1.98,0.003,5,1000);
 	PIDturn(25,1.98,0.0035,5,1000);
-	intake = 127;
 	delay(200);
 	wings.set_value(false);
 	delay(200);
 	PIDdrive(5,0.5,0.01,5,1000);
+	intake = 127;
 	delay(200);
-	PIDturn(-20,1.98,0.003,5,1000);
-	PIDdrive(25, 0.5, 0.01, 5, 1000);
+	PIDturn(-32,1.98,0.003,5,1000);
+	delay(200);
+	PIDdrive(35, 0.5, 0.01, 5, 1000);
+
 
 	
 	
